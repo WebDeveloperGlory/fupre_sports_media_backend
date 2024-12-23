b79a8dd2e1d15ff228a6f80a41e7cfa4f7f8e245
WebDeveloperGlory <gloryumuerri223@gmail.com>
2024-12-23 01:16:37 -0800

    update user route path again

--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -7,7 +7,7 @@ const router = Router();
 router.get( '/', controller.getAllUsers );
 router.post( '/register', controller.createUser );
 router.post( '/login', controller.loginUser );
-router.get( '/:userId/profile', controller.getUserProfile );
-router.delete( '/:userId/delete', controller.deleteUser );
+router.get( '/:userId', controller.getUserProfile );
+router.delete( '/:userId', controller.deleteUser );
 
 module.exports = router;
