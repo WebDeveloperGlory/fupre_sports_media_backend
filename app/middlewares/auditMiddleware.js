d2b56917bd12b538f7c6f13993d8323893e71c2a
WebDeveloperGlory <gloryumuerri223@gmail.com>
2025-03-13 21:36:29 -0700

    create audit log middleware

---
+++ app/middlewares/auditMiddleware.js
@@ -0,0 +1,63 @@
+const auditService = require('../services/general/auditLogService');
+const { error, serverError } = require('../utils/responseUtils');
+
+const logRequest = ( req, res, next ) => {
+    // Store original methods to intercept them later
+    const originalSend = res.send;
+    const originalJson = res.json;
+
+    // Add request info to req object
+    req.auditInfo = {
+        ipAddress: req.ip,
+        userAgent: req.get('User-Agent')
+    };
+
+    // Intercept response methods
+    res.send = function ( body ) {
+      res.responseBody = body;
+      return originalSend.call( this, body );
+    };
+
+    res.json = function ( body ) {
+      res.responseBody = body;
+      return originalJson.call( this, body );
+    };
+
+    next();
+}
+
+const logActionManually = async (
+    req, res, 
+    {
+        action, entity, 
+        entityId, details = {}, 
+        previousValues = {}, newValues = {}
+    }
+) => {
+    // Check for user in request
+    if ( !req.user ) {
+        console.warn('No user found in request for audit logging');
+        return;
+    }
+
+    try {
+        await auditService.createAuditLog({
+            userId: req.user._id,
+            action,
+            entity,
+            entityId,
+            details,
+            previousValues,
+            newValues,
+            requestInfo: req.auditInfo
+        });
+    } catch ( error ) {
+        console.error('Error in audit logging:', error);
+    }
+}
+
+module.exports = {
+    logRequest,
+    logActionManually,
+    
+};
