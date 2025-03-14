dacda6a31375490ea944e4775220f023e28e4919
WebDeveloperGlory <gloryumuerri223@gmail.com>
2025-03-13 20:57:11 -0700

    create general auditlog model

---
+++ app/models/general/AuditLog.js
@@ -0,0 +1,46 @@
+const { Schema } = require('mongoose');
+
+const auditLogSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    action: {
+        type: String,
+        required: true,
+        enum: ['CREATE', 'UPDATE', 'DELETE', 'LOGIN', 'LOGOUT']
+    },
+    entity: {
+        type: String,
+        required: true // e.g., 'Competition', 'Team', 'Match', 'Player'
+    },
+    entityId: {
+        type: Schema.Types.ObjectId,
+        required: true
+    },
+    details: {
+        type: Object,
+        default: {} // For storing additional information
+    },
+    previousValues: {
+        type: Object,
+        default: {} // For storing previous values in case of updates
+    },
+    newValues: {
+        type: Object,
+        default: {} // For storing new values in case of updates
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    },
+    ipAddress: {
+        type: String
+    },
+    userAgent: {
+        type: String
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model( 'AuditLog', auditLogSchema );
