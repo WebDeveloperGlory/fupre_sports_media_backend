167a3eedc46611bf713a19da6d5d1867b55d9202
WebDeveloperGlory <gloryumuerri223@gmail.com>
2025-03-15 11:52:26 -0700

    refactor player permissions middleware and use in routes

--- app/routes/football/footballPlayerRoutes.js
+++ app/routes/football/footballPlayerRoutes.js
@@ -1,14 +1,14 @@
 const { Router } = require('express');
 const controller = require('../../controllers/football/footballPlayerController');
 const { authenticateUser } = require('../../middlewares/authMiddleware');
-const { hasPlayerPermissions, authorize } = require('../../middlewares/adminMiddleware');
+const { refactoredHasPlayerPermisions, authorize } = require('../../middlewares/adminMiddleware');
 
 const router = Router();
 
 router.post('/', authenticateUser, authorize([ 'super-admin', 'team-admin' ]), controller.addPlayers);
 router.get('/:playerId', controller.getPlayer);
-router.put('/:playerId', authenticateUser, hasPlayerPermissions, controller.updateTeamPlayer);
-router.delete('/:playerId', authenticateUser, hasPlayerPermissions, controller.deleteTeamPlayer);
-router.put('/:playerId/records', authenticateUser, hasPlayerPermissions, controller.updatePlayerRecords);
+router.put('/:playerId', authenticateUser, refactoredHasPlayerPermisions, controller.updateTeamPlayer);
+router.delete('/:playerId', authenticateUser, refactoredHasPlayerPermisions, controller.deleteTeamPlayer);
+router.put('/:playerId/records', authenticateUser, refactoredHasPlayerPermisions, controller.updatePlayerRecords);
 
 module.exports = router;
