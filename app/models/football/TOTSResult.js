d16861790351c261661865e214e5e1defe734ce0
WebDeveloperGlory <gloryumuerri223@gmail.com>
2025-03-31 12:38:23 -0700

    new tots service and tots result changes

--- app/models/football/TOTSResult.js
+++ app/models/football/TOTSResult.js
@@ -6,63 +6,86 @@ const teamOfTheSeasonResultSchema = new Schema({
         ref: 'FootballTOTSSession',
         required: true
     },
-    votersChoice: {
-        formation: { type: 'String' },
-        GK: {
-            type: Schema.Types.ObjectId,
-            ref: 'FootballPlayer'
-        },
-        DEF: [{
-            type: Schema.Types.ObjectId,
-            ref: 'FootballPlayer'
-        }],
-        MID: [{
-            type: Schema.Types.ObjectId,
-            ref: 'FootballPlayer'
-        }],
-        FWD: [{
-            type: Schema.Types.ObjectId,
-            ref: 'FootballPlayer'
-        }],
+    // Formation data
+    winningFormation: { 
+        type: String, 
+        required: true 
     },
+    winningFormationPercentage: { 
+        type: Number, 
+        required: true 
+    },
+    
+    // Full voting results data
+    winningPlayers: {
+        GK: [{ type: Schema.Types.ObjectId, ref: 'FootballPlayer' }],
+        DEF: [{ type: Schema.Types.ObjectId, ref: 'FootballPlayer' }],
+        MID: [{ type: Schema.Types.ObjectId, ref: 'FootballPlayer' }],
+        FWD: [{ type: Schema.Types.ObjectId, ref: 'FootballPlayer' }]
+    },
+    winningPlayersVotes: {
+        GK: { type: Number, default: 0 },
+        DEF: { type: Number, default: 0 },
+        MID: { type: Number, default: 0 },
+        FWD: { type: Number, default: 0 }
+    },
+    winningPlayersPercentage: {
+        GK: { type: Number, default: 0 },
+        DEF: { type: Number, default: 0 },
+        MID: { type: Number, default: 0 },
+        FWD: { type: Number, default: 0 }
+    },
+    
+    // Admin choice data
     adminChoice: {
-        formation: { type: 'String' },
-        GK: {
-            type: Schema.Types.ObjectId,
-            ref: 'FootballPlayer'
-        },
-        DEF: [{
-            type: Schema.Types.ObjectId,
-            ref: 'FootballPlayer'
-        }],
-        MID: [{
-            type: Schema.Types.ObjectId,
-            ref: 'FootballPlayer'
-        }],
-        FWD: [{
-            type: Schema.Types.ObjectId,
-            ref: 'FootballPlayer'
-        }],
+        GK: [{ type: Schema.Types.ObjectId, ref: 'FootballPlayer' }],
+        DEF: [{ type: Schema.Types.ObjectId, ref: 'FootballPlayer' }],
+        MID: [{ type: Schema.Types.ObjectId, ref: 'FootballPlayer' }],
+        FWD: [{ type: Schema.Types.ObjectId, ref: 'FootballPlayer' }]
+    },
+    adminChoiceVotes: {
+        GK: { type: Number, default: 0 },
+        DEF: { type: Number, default: 0 },
+        MID: { type: Number, default: 0 },
+        FWD: { type: Number, default: 0 }
+    },
+    adminChoicePercentage: {
+        GK: { type: Number, default: 0 },
+        DEF: { type: Number, default: 0 },
+        MID: { type: Number, default: 0 },
+        FWD: { type: Number, default: 0 }
+    },
+    
+    // Final winners (combines the structure from your original schema)
+    finalWinners: {
+        GK: { type: Schema.Types.ObjectId, ref: 'FootballPlayer' },
+        DEF: { type: Schema.Types.ObjectId, ref: 'FootballPlayer' },
+        MID: { type: Schema.Types.ObjectId, ref: 'FootballPlayer' },
+        FWD: { type: Schema.Types.ObjectId, ref: 'FootballPlayer' }
+    },
+    
+    // Legacy/compatibility fields that match your original structure
+    votersChoice: {
+        formation: { type: String },
+        GK: { type: Schema.Types.ObjectId, ref: 'FootballPlayer' },
+        DEF: [{ type: Schema.Types.ObjectId, ref: 'FootballPlayer' }],
+        MID: [{ type: Schema.Types.ObjectId, ref: 'FootballPlayer' }],
+        FWD: [{ type: Schema.Types.ObjectId, ref: 'FootballPlayer' }]
+    },
+    adminChoice_legacy: {
+        formation: { type: String },
+        GK: { type: Schema.Types.ObjectId, ref: 'FootballPlayer' },
+        DEF: [{ type: Schema.Types.ObjectId, ref: 'FootballPlayer' }],
+        MID: [{ type: Schema.Types.ObjectId, ref: 'FootballPlayer' }],
+        FWD: [{ type: Schema.Types.ObjectId, ref: 'FootballPlayer' }]
     },
     finalWinner: {
-        formation: { type: 'String' },
-        GK: {
-            type: Schema.Types.ObjectId,
-            ref: 'FootballPlayer'
-        },
-        DEF: [{
-            type: Schema.Types.ObjectId,
-            ref: 'FootballPlayer'
-        }],
-        MID: [{
-            type: Schema.Types.ObjectId,
-            ref: 'FootballPlayer'
-        }],
-        FWD: [{
-            type: Schema.Types.ObjectId,
-            ref: 'FootballPlayer'
-        }],
-    },
+        formation: { type: String },
+        GK: { type: Schema.Types.ObjectId, ref: 'FootballPlayer' },
+        DEF: [{ type: Schema.Types.ObjectId, ref: 'FootballPlayer' }],
+        MID: [{ type: Schema.Types.ObjectId, ref: 'FootballPlayer' }],
+        FWD: [{ type: Schema.Types.ObjectId, ref: 'FootballPlayer' }]
+    }
 }, { timestamps: true });
 
-module.exports = mongoose.model( 'FootballTOTSResult', teamOfTheSeasonResultSchema );
+module.exports = mongoose.model('FootballTOTSResult', teamOfTheSeasonResultSchema);
