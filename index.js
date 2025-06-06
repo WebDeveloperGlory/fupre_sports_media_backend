be1704a119dc75269415bd524380ab9701ac2db8
WebDeveloperGlory <gloryumuerri223@gmail.com>
2025-06-06 11:57:04 -0700

    move v1 and current v2 files into a v1 folder in app and rename current v2 to v1.2 to accommodate the actual v2 routes

--- index.js
+++ index.js
@@ -2,37 +2,37 @@ const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
-const { swaggerSpecV1, swaggerSpecV2 } = require('./app/config/swagger');
-const { PORT, ALLOWED_ORIGINS } = require('./app/config/env');
-const { initializeWebSocket } = require('./app/config/socket');
+const { swaggerSpecV1, swaggerSpecV2 } = require('./app/v1/config/swagger');
+const { PORT, ALLOWED_ORIGINS } = require('./app/v1/config/env');
+const { initializeWebSocket } = require('./app/v1/config/socket');
 
 // V1 ROUTES //
-const authRoutes = require('./app/routes/authRoutes');
-const teamRoutes = require('./app/routes/teamRoutes');
-const fixtureRoutes = require('./app/routes/fixtureRoutes');
-const competitionRoutes = require('./app/routes/competitionRoutes');
-const adminRoutes = require('./app/routes/adminRoutes');
-const liveFixtureRoutes = require('./app/routes/liveFixtureRoutes');
-const generalRoutes = require('./app/routes/generalRoutes');
-const playerRoutes = require('./app/routes/playerRoutes');
-const totsRoutes = require('./app/routes/TOTSRoutes');
+const authRoutes = require('./app/v1/routes/authRoutes');
+const teamRoutes = require('./app/v1/routes/teamRoutes');
+const fixtureRoutes = require('./app/v1/routes/fixtureRoutes');
+const competitionRoutes = require('./app/v1/routes/competitionRoutes');
+const adminRoutes = require('./app/v1/routes/adminRoutes');
+const liveFixtureRoutes = require('./app/v1/routes/liveFixtureRoutes');
+const generalRoutes = require('./app/v1/routes/generalRoutes');
+const playerRoutes = require('./app/v1/routes/playerRoutes');
+const totsRoutes = require('./app/v1/routes/TOTSRoutes');
 // END OF V1 ROUTES //
 
-// V2 ROUTES //
-const authenticationRoutes = require('./app/routes/general/authRoutes');
-const notificationRoutes = require('./app/routes/general/notificationRoutes');
-const userRoutes = require('./app/routes/general/userRoutes');
-const auditRoutes = require('./app/routes/general/auditLogRoutes');
-const footballPlayerRoutes = require('./app/routes/football/footballPlayerRoutes');
-const footballFixtureRoutes = require('./app/routes/football/footballFixtureRoutes');
-const footballTeamRoutes = require('./app/routes/football/footballTeamRoutes');
-const footballCompetitionRoutes = require('./app/routes/football/footballCompetitionRoutes');
-const footballTOTSRoutes = require('./app/routes/football/footballTOTSRoutes');
-// END OF V2 ROUTES //
+// V1.2 ROUTES //
+const authenticationRoutes = require('./app/v1/routes/general/authRoutes');
+const notificationRoutes = require('./app/v1/routes/general/notificationRoutes');
+const userRoutes = require('./app/v1/routes/general/userRoutes');
+const auditRoutes = require('./app/v1/routes/general/auditLogRoutes');
+const footballPlayerRoutes = require('./app/v1/routes/football/footballPlayerRoutes');
+const footballFixtureRoutes = require('./app/v1/routes/football/footballFixtureRoutes');
+const footballTeamRoutes = require('./app/v1/routes/football/footballTeamRoutes');
+const footballCompetitionRoutes = require('./app/v1/routes/football/footballCompetitionRoutes');
+const footballTOTSRoutes = require('./app/v1/routes/football/footballTOTSRoutes');
+// END OF V1.2 ROUTES //
 
-// V2 VIEWS ROUTES //
-const viewRoutes = require('./app/routes/views/viewRoutes');
-// END OF V2 VIEWS ROUTES //
+// V1.2 VIEWS ROUTES //
+const viewRoutes = require('./app/v1/routes/views/viewRoutes');
+// END OF V1.2 VIEWS ROUTES //
 
 const app = express();
 const APP_PORT = PORT;
@@ -57,7 +57,7 @@ app.use( cors( corsOptions ) );
 app.use( express.json() );
 
 app.use( '/api/v1/api-docs', swaggerUi.serve, swaggerUi.setup( swaggerSpecV1 ) );
-app.use( '/api/v2/api-docs', swaggerUi.serve, swaggerUi.setup( swaggerSpecV2 ) );
+app.use( '/api/v1.2/api-docs', swaggerUi.serve, swaggerUi.setup( swaggerSpecV2 ) );
 
 initializeWebSocket( server, corsOptions );
 app.use((req, res, next) => {
@@ -78,21 +78,21 @@ app.get( '/', ( req, res ) => {
 });
 //END OF TEST ROUTES //
 
-// V2 ROUTES //
-app.use( '/api/v2/authentication', authenticationRoutes );
-app.use( '/api/v2/notification', notificationRoutes );
-app.use( '/api/v2/user', userRoutes );
-app.use( '/api/v2/audit', auditRoutes );
-app.use( '/api/v2/football/player', footballPlayerRoutes );
-app.use( '/api/v2/football/fixture', footballFixtureRoutes );
-app.use( '/api/v2/football/team', footballTeamRoutes );
-app.use( '/api/v2/football/competition', footballCompetitionRoutes );
-app.use( '/api/v2/football/tots', footballTOTSRoutes );
-// END OF V2 ROUTES //
+// V1.2 ROUTES //
+app.use( '/api/v1.2/authentication', authenticationRoutes );
+app.use( '/api/v1.2/notification', notificationRoutes );
+app.use( '/api/v1.2/user', userRoutes );
+app.use( '/api/v1.2/audit', auditRoutes );
+app.use( '/api/v1.2/football/player', footballPlayerRoutes );
+app.use( '/api/v1.2/football/fixture', footballFixtureRoutes );
+app.use( '/api/v1.2/football/team', footballTeamRoutes );
+app.use( '/api/v1.2/football/competition', footballCompetitionRoutes );
+app.use( '/api/v1.2/football/tots', footballTOTSRoutes );
+// END OF V1.2 ROUTES //
 
-// V2 VIEWS ROUTES //
-app.use( '/api/v2/views', viewRoutes );
-// END OF V2 VIEWS ROUTES //
+// V1.2 VIEWS ROUTES //
+app.use( '/api/v1.2/views', viewRoutes );
+// END OF V1.2 VIEWS ROUTES //
 
 // V1 ROUTES //
 app.use( '/api/auth', authRoutes );
@@ -113,6 +113,6 @@ app.use( '/api/tots', totsRoutes );
 server.listen( PORT, () => {
     console.log(`Fupre Sports Media Server Running On Port: ${ APP_PORT }`);
     console.log(`Swagger Docs Version 1 Available At: ${ APP_PORT }/api/v1/api-docs`);
-    console.log(`Swagger Docs Version 2 Available At: ${ APP_PORT }/api/v2/api-docs`);
+    console.log(`Swagger Docs Version 2 Available At: ${ APP_PORT }/api/v1.2/api-docs`);
     console.log(`Websockets server running on port ${ APP_PORT }`);
 });
